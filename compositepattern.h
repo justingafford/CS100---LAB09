9ae03cfdcf606b27e9ff8da393ba5cc3fe6ee53c
Justin Gafford <jgaff001@ucr.edu>
2019-06-07 11:58:20 -0700

    Update and rename compositepattern.hpp to compositepattern.h

--- compositepattern.h
+++ compositepattern.h
@@ -1,20 +1,24 @@
-#ifndef compositepattern_h
-#define compositepattern_h
+#ifndef COMPOSITE_HEADER
+#define COMPOSITE_HEADER
 
 #include <iostream>
 #include <sstream>
 #include <math.h>
 #include <string>
-#include "iterator.hpp"
+#include "iterator.h"
 
 using namespace std;
 
 class Iterator;
 class NullIterator;
 class UnaryIterator;
+
 //Abstract Base Class
 class Base {
     public:
+        Base(){};
+
+        //virtual
         virtual string stringify() = 0;
         virtual double evaluate() = 0;
         virtual Iterator* create_iterator() = 0;
@@ -22,30 +26,19 @@ class Base {
         virtual Base* get_right() = 0;
 };
 
-//Leaf Classes
+//Leaf Class
 class Op: public Base {
-   protected:
-        double num;
-   public:
-	Op();
-        Op(double n);
-        double evaluate();
-        string stringify();
-        Base* get_left();
-        Base* get_right();  
-        Iterator* create_iterator();
-};
-
-class Rand : public Base
-{
     private:
-        double num;
+        double value;
+
     public:
-        Rand();
-        double evaluate();
-        string stringify();
+        Op();
+        Op(double val);
+
         Base* get_left();
-        Base* get_right();  
+        Base* get_right();
+        double evaluate(); 
+        string stringify();
         Iterator* create_iterator();
 };
 
@@ -56,9 +49,10 @@ class Operator: public Base {
     public:
         Operator();
         Operator(Base* l, Base* r);
+
         Base* get_left();
         Base* get_right();
-        virtual double evaluate() = 0;
+        virtual double evaluate() = 0;	//Note: this is implicit in the inheritance, but can also be made explicit
         Iterator* create_iterator();
 };
 
@@ -68,60 +62,56 @@ class UnaryOperator: public Base {
     public:
         UnaryOperator();
         UnaryOperator(Base* c);
+
         Base* get_left();
         Base* get_right();
-        virtual double evaluate() = 0;
+        virtual double evaluate() = 0;	//Note: this is implicit in the inheritance, but can also be made explicit
         Iterator* create_iterator();
 };
 
+//Composite Classes
 class Add: public Operator {
- private:
-	Base* L1;
-	Base* L2;
- public:
-	Add(Base*, Base*);
-	double evaluate();
-	string stringify();
+    public:
+        Add();
+        Add(Base* left, Base* right);
+
+        string stringify();
+        double evaluate();
 };
 
 class Sub: public Operator {
-  private:
-	 Base* L1;
-	 Base* L2;
-  public:
-	 Sub(Base*, Base*);
-	 double evaluate();
-	 string stringify();
+    public:
+        Sub();
+        Sub(Base* left, Base* right);
+
+        string stringify();
+        double evaluate();
 };
 
 class Mult: public Operator {
- private:
-	Base* L1;
-	Base* L2;      
- public:
-  Mult(Base*, Base*);
-	double evaluate();
-	string stringify();
+    public:
+        Mult();
+        Mult(Base* left, Base* right);
+
+        string stringify();
+        double evaluate();
 };
 
 class Div: public Operator {
-  private:
-	 Base* L1;
-	 Base* L2;
-  public:
-	 Div(Base*, Base*);
-	 double evaluate();
-	 string stringify();
-};
+     public:
+        Div();
+        Div(Base* left, Base* right);
 
-class Pow: public UnaryOperator {
-  private:
-	  Base* L1;
-	  Base* L2;   
-  public:
-    Pow(Base*, Base*);
-	  double evaluate();
-	  string stringify();
+        string stringify();
+        double evaluate();
+};
+class Pow: public Operator {
+    public:
+        Pow();
+        Pow(Base* left, Base* right);
+
+        string stringify();
+        double evaluate();
 };
 
 class Root: public UnaryOperator {
