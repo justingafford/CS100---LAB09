a01d0712d703c5fecbc346e742e6bfc47dbacde4
Justin Gafford <jgaff001@ucr.edu>
2019-06-06 22:33:16 -0700

    Update unit_test.cpp

---
+++ unit_test.cpp
@@ -0,0 +1,9 @@
+#include "compositepattern.hpp"
+#include "iterator.hpp"
+#include "#include <iostream>
+#include "gtest/gtest.h"
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
